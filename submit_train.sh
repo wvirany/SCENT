1dcc9c04a142b5de51a06a4ce7044ee7f6579769
wvirany <waltervirany@gmail.com>
2025-10-15 10:14:41 -0400

    Medium train configs

--- submit_train.sh
+++ submit_train.sh
@@ -3,4 +3,18 @@
 #SBATCH --mem=32G
 #SBATCH --cpus-per-task=4
 #SBATCH --gres=gpu:l40s:1
-#SBATCH --job-name=scent_
+#SBATCH --job-name=scenta_train_medium
+#SBATCH --output=logs/train_medium_%A_%a.out
+#SBATCH --error=logs/train_medium_%A_%a.err
+#SBATCH --array=1-8
+#SBATCH --requeue
+
+# Load modules
+module load python/3.11 scipy-stack rdkit/2023.09.5 cuda/12.6
+
+# Activate virtual environment
+source ~/scratch/SCENT/scent_env/bin/activate
+
+# Run experiment
+cd ~/scratch/SCENT
+python train.py --cfg configs/experiments/train_medium.gin
